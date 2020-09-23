bb7dd1e75f341cac0572238b0abca26d4f54cf02
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-09-23 21:07:29 +0530

    bp

--- src/main/java/com/fb/demo/entity/Tenant.java
+++ src/main/java/com/fb/demo/entity/Tenant.java
@@ -1,5 +1,33 @@
 package com.fb.demo.entity;
 
-public class Tenant {
+import java.io.Serializable;
+import java.util.Map;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@Entity(name = "Tenant")
+@lombok.Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Table(name = "tenant", uniqueConstraints = {@UniqueConstraint(columnNames = {"name"})})
+public class Tenant implements Serializable {
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  @Column(name = "name", unique = true, nullable = false, updatable = false)
+  private String name;
+  private String description;
+  private String version;
+  private String url;
+  private boolean isActive;
+  private String organizationName;
+  private String registrationNumber;
+  private String website;
+  private String tenantType;
 }
---
+++ src/main/java/com/fb/demo/repository/TenantRepository.java
@@ -0,0 +1,10 @@
+package com.fb.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.fb.demo.entity.Tenant;
+
+@Repository
+public interface TenantRepository extends JpaRepository<Tenant, Integer> {
+
+}
