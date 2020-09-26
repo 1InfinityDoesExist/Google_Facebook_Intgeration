1f4f2197e6e92d5bf071dfa5bfeb2d38b53c1435
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-09-26 16:56:50 +0530

    swagger

--- src/main/java/com/fb/demo/entity/FbDeveloperDetails.java
+++ src/main/java/com/fb/demo/entity/FbDeveloperDetails.java
@@ -3,6 +3,8 @@ package com.fb.demo.entity;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,15 +12,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel(value = "FbDevCred")
 public class FbDeveloperDetails extends BaseEntity implements Serializable {
 
     /**
      * 
      */
     private static final long serialVersionUID = 1L;
+    @ApiModelProperty(value = "clientId", notes = "Parent Tenant")
     private Integer parentTenant;
+    @ApiModelProperty(notes = "Client Id")
     private String clientId;
+    @ApiModelProperty(notes = "Client Secret")
     private String clientSecret;
+    @ApiModelProperty(notes = "Redirect Url")
     private String redirectUrl;
 
 }
