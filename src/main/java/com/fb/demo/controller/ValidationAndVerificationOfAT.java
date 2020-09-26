4f1fa8d9cba134fea00a9c5191d3f012c40fa1d4
Avinash Patel <avinash.patel@gaiansolutions.com>
2020-09-26 12:25:49 +0530

    verification and validation of accessToken

---
+++ src/main/java/com/fb/demo/controller/ValidationAndVerificationOfAT.java
@@ -0,0 +1,76 @@
+package com.fb.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.fb.demo.exception.FbAccessTokenValidationException;
+import com.fb.demo.exception.FbDeveloperDetailsNotFoundException;
+import com.fb.demo.exception.TenantNotFoundException;
+import com.fb.demo.service.ValidationAndVerificationOfATService;
+import com.fb.demo.service.impl.InvalidInputException;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController("v1ValidationAndVerificationOfAccessToken")
+@RequestMapping(path = "/verfiyAndValidate")
+@Slf4j
+public class ValidationAndVerificationOfAT {
+
+
+    @Autowired
+    private ValidationAndVerificationOfATService validationAndVerificationOfATService;
+
+
+    @GetMapping(path = "/google")
+    @ApiImplicitParams({@ApiImplicitParam(name = "accessToken", paramType = "path")})
+    public ResponseEntity<?> verifyAndValidateGoogleAccessToken(
+                    @RequestParam(name = "accessToken", required = true) String accessToken)
+                    throws Exception {
+        try {
+            validationAndVerificationOfATService.verifyAndValidateGoogleAccessToken(accessToken);
+        } catch (Exception ex) {
+
+        }
+        return null;
+    }
+
+
+    @GetMapping(path = "/facebook/{tenant}")
+    @ApiImplicitParams({@ApiImplicitParam(name = "accessToken", paramType = "path"),
+                @ApiImplicitParam(name = "tenant", paramType = "path")})
+    public ResponseEntity<?> verifyAndValidateFacebookAccessToken(
+                    @RequestParam(name = "accessToken", required = true) String accessToken,
+                    @PathVariable(name = "tenant", required = true) String tenant)
+                    throws Exception {
+        try {
+            validationAndVerificationOfATService.verifyAndValidateFacebookAccessToken(accessToken,
+                            tenant);
+            return ResponseEntity.status(HttpStatus.OK)
+                            .body(new ModelMap().addAttribute("validation", "success")
+                                            .addAttribute("verification", "success"));
+        } catch (final InvalidInputException ex) {
+            log.error(":::::Invalid input, access token must not be null or empty::::");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
+        } catch (final FbAccessTokenValidationException ex) {
+            log.error(":::::Invalid Access Token:::::");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
+        } catch (final TenantNotFoundException ex) {
+            log.error(":::::Tenant does not exist:::::");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
+        } catch (final FbDeveloperDetailsNotFoundException ex) {
+            log.error("::::FbDeveloper not found:::::");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                            .body(new ModelMap().addAttribute("msg", ex.getMessage()));
+        }
+    }
+}
---
+++ src/main/java/com/fb/demo/service/impl/ValidationAndVerificationOfATServiceImpl.java
@@ -0,0 +1,150 @@
+package com.fb.demo.service.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import com.fb.demo.entity.FbDeveloperDetails;
+import com.fb.demo.entity.Tenant;
+import com.fb.demo.exception.FbAccessTokenValidationException;
+import com.fb.demo.exception.FbDeveloperDetailsNotFoundException;
+import com.fb.demo.exception.TenantNotFoundException;
+import com.fb.demo.repository.FbDeveloperDetailsRepository;
+import com.fb.demo.repository.TenantRepository;
+import com.fb.demo.service.ValidationAndVerificationOfATService;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ValidationAndVerificationOfATServiceImpl
+                implements ValidationAndVerificationOfATService {
+
+    @Autowired
+    private TenantRepository tenantRepository;
+
+    @Autowired
+    private FbDeveloperDetailsRepository fbDeveloperDetailsRepository;
+
+    @Value("${facebook.token.verification.url}")
+    public String facebookTokenVerficationUrl;
+
+    @Value("${facebook.dummy.access.token.generator.url}")
+    public String facebookAccessTokenGeneratiorUrl;
+
+    @Value("${facebook.dummy.access.token.verification.url}")
+    public String facebookAccessTokenValidatorUrl;
+
+    @Override
+    public void verifyAndValidateFacebookAccessToken(String accessToken, String tenant)
+                    throws Exception {
+        log.info(":::::AccessToken {} and Tenant {}", accessToken, tenant);
+        if (accessToken == null || accessToken.isEmpty()) {
+            throw new InvalidInputException("Invalid accessToken.");
+        }
+        StringBuffer buffer;
+        String details = null;
+        try {
+            log.info("Facebook Token Verification Url : {}", facebookTokenVerficationUrl);
+            URL url = new URL(facebookTokenVerficationUrl + accessToken);
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(urlConnection.getInputStream()));
+            buffer = new StringBuffer();
+            String inputLine;
+            while ((inputLine = bufferedReader.readLine()) != null) {
+                buffer.append(inputLine);
+            }
+            bufferedReader.close();
+            details = buffer.toString();
+            log.info(":::::details {}", details);
+
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(details);
+            for (Object obj : jsonObject.keySet()) {
+                String param = (String) obj;
+                if (param.equalsIgnoreCase("error")) {
+                    throw new FbAccessTokenValidationException(
+                                    (String) (((JSONObject) jsonObject.get(param)).get("message")));
+                }
+                if (param.equalsIgnoreCase("id")) {
+                    if (verifiyFacebookAccessToken(accessToken, tenant)) {
+                        return;
+                    }
+                }
+            }
+        } catch (final IOException ex) {
+            log.error(":::::Unable to connect to the facebookTokenVerificationUrl:::::");
+        }
+    }
+
+    private boolean verifiyFacebookAccessToken(String accessToken, String tenant) throws Exception {
+        log.info("::::::Varifying input accesstoken with actual accessToken::::");
+        Tenant tenantFromDB = tenantRepository.getTenantByName(tenant);
+        if (tenantFromDB == null) {
+            throw new TenantNotFoundException("Tenant not found.");
+        }
+        FbDeveloperDetails fbDeveloperDetails =
+                        fbDeveloperDetailsRepository.findByParentTenant(tenantFromDB.getId());
+        if (fbDeveloperDetails == null) {
+            throw new FbDeveloperDetailsNotFoundException("FbDeveloperDetails not found.");
+        }
+
+        // Generating Actual Dummy Access Token Using clientId, Client Secret, and grant_type
+        HttpResponse<JsonNode> jsonNode = Unirest.get(facebookAccessTokenGeneratiorUrl)
+                        .header("Accept", "application/json")
+                        .header("Content-Type", "application/json")
+                        .queryString("client_id", fbDeveloperDetails.getClientId())
+                        .queryString("client_secret", fbDeveloperDetails.getClientSecret())
+                        .queryString("grant_type", "client_credentials").asJson();
+
+        org.json.JSONObject jsonObject = jsonNode.getBody().getObject();
+        for (Object obj : jsonObject.keySet()) {
+            String param = (String) obj;
+            if (param.equalsIgnoreCase("error")) {
+                throw new InvalidInputException("Invalid Input.");
+            }
+            if (param.equalsIgnoreCase("access_token")) {
+                // Verifying the dummy access token with the input_access_token
+                HttpResponse<JsonNode> finalJsonNode = Unirest.get(facebookAccessTokenValidatorUrl)
+                                .header("Accept", "application/json")
+                                .header("Content-Type", "application/json")
+                                .queryString("input_token", accessToken)
+                                .queryString("access_token", jsonObject.get(param)).asJson();
+                org.json.JSONObject finalJsonObject = finalJsonNode.getBody().getObject();
+                for (Object object : finalJsonObject.keySet()) {
+                    String key = (String) object;
+                    if (key.equalsIgnoreCase("data")) {
+                        org.json.JSONObject jsonData =
+                                        (org.json.JSONObject) finalJsonObject.get(key);
+                        if (!jsonData.isNull("error")) {
+                            log.error("::::::Errro message {}", jsonData.get("message"));
+                            return false;
+                        }
+                        for (Object finalObject : jsonData.keySet()) {
+                            String finalParam = (String) finalObject;
+                            if (finalParam.equalsIgnoreCase("app_id")) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void verifyAndValidateGoogleAccessToken(String accessToken) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
